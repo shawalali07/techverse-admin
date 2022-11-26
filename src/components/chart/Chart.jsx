77d37fd562246c95ed0c8e385cdea44a3de60cca
Shawal Ali <shawalnetwork@gmail.com>
2022-11-26 15:06:52 +0500

    xaxis resolved

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -14,7 +14,7 @@ export default function Chart({ title, data, dataKey, grid }) {
       <h3 className='chartTitle'>{title}</h3>
       <ResponsiveContainer width='100%' aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey='_id' stroke='#5550bd' />
+          <XAxis interval='preserveStartEnd' dataKey='_id' stroke='#5550bd' />
           <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
           <Tooltip />
           {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
